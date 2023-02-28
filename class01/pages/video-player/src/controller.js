56eeed1bd3f6765bf83af99112a87e06c9c14a4f
RCristyan <rcristyan9@gmail.com>
2023-02-28 14:51:28 -0300

    Implementado log ao interagir com botão de detecção

--- class01/pages/video-player/src/controller.js
+++ class01/pages/video-player/src/controller.js
@@ -1,14 +1,28 @@
 export default class Controller {
-    constructor({}) {
+    #view
+    #service
+    constructor({ view, service }) {
+        this.#view = view
+        this.#service = service
 
+        this.#view.configureOnBtnClick(this.onBtnStart.bind(this))
     }
 
     static async initialize(deps) {
         const controller = new Controller(deps)
+        controller.log("Blink detection not has yet started. Click on the button to continue")
         return controller.init()
     }
 
     async init () {
         console.log('init')
+    }
+
+    log(text) {
+        this.#view.log(`log: ${text}`)
+    }
+
+    onBtnStart() {
+        this.log('Initializing detection...')
     }
 }
