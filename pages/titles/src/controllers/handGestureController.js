ca725e867637dc27d45dae2d1ee41f9cba24a9a1
RCristyan <rcristyan9@gmail.com>
2023-03-02 17:21:26 -0300

    Implementada integração inicial entre camera e detector de mãos

--- pages/titles/src/controllers/handGestureController.js
+++ pages/titles/src/controllers/handGestureController.js
@@ -1,13 +1,29 @@
 export default class HandGestureController {
+    #camera
     #view
     #service
-    constructor({ view, service }){
+    constructor({ camera, view, service }){
+        this.#camera = camera
         this.#view = view
         this.#service = service
     }
 
     async init() {
+        return this.#loop()
+    }
+
+    async #estimateHands() {
+        try {
+            const hands = await this.#service.estimateHands(this.#camera.video)
+            console.log({ hands })
+        } catch (error) {
+            console.log('Erro:', error)
+        }
+    }
+    
+    async #loop() {
         await this.#service.initializeDetector()
+        await this.#estimateHands()
     }
 
     static async initialize(deps) {
