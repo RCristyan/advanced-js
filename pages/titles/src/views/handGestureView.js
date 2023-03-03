9082bd60313ff16744095bdee4ad07ce7b218b6a
RCristyan <rcristyan9@gmail.com>
2023-03-03 18:54:24 -0300

    Implementado canvas e métodos iniciais

--- pages/titles/src/views/handGestureView.js
+++ pages/titles/src/views/handGestureView.js
@@ -1,4 +1,20 @@
 export default class HandGestureView {
+    #handsCanvas = document.querySelector('#hands')
+    #canvasContext = this.#handsCanvas.getContext('2d')
+
+    constructor() {
+        this.#handsCanvas.width = globalThis.screen.availWidth
+        this.#handsCanvas.height = globalThis.screen.availHeight
+    }
+
+    clearCanvas() {
+        this.#canvasContext.clearRect(0, 0, this.#handsCanvas.width, this.#handsCanvas.height)
+    }
+
+    drawResults(hands) {
+
+    }
+
     loop(fn) {
         requestAnimationFrame(fn)
     }
