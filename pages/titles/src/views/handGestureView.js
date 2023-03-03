75fecb5aff067750dbacff9e72680a284c506b2e
RCristyan <rcristyan9@gmail.com>
2023-03-03 19:37:19 -0300

    Adicionado desenho de mãos

--- pages/titles/src/views/handGestureView.js
+++ pages/titles/src/views/handGestureView.js
@@ -1,10 +1,12 @@
 export default class HandGestureView {
     #handsCanvas = document.querySelector('#hands')
     #canvasContext = this.#handsCanvas.getContext('2d')
+    #fingerLookupIndexes
 
-    constructor() {
+    constructor({ fingerLookupIndexes }) {
         this.#handsCanvas.width = globalThis.screen.availWidth
         this.#handsCanvas.height = globalThis.screen.availHeight
+        this.#fingerLookupIndexes = fingerLookupIndexes
     }
 
     clearCanvas() {
@@ -21,6 +23,7 @@ export default class HandGestureView {
             this.#canvasContext.lineJoin = "round"
 
             this.#drawJoints(keypoints)
+            this.#drawFingersAndHoverElements(keypoints)
         }
     }
 
@@ -36,6 +39,27 @@ export default class HandGestureView {
 
             this.#canvasContext.arc(newX, newY, radius, startAngle, endAngle)
             this.#canvasContext.fill()
+        }
+    }
+
+    #drawFingersAndHoverElements(keypoints) {
+        const fingers = Object.keys(this.#fingerLookupIndexes)
+
+        for(const finger of fingers) {
+            const points = this.#fingerLookupIndexes[finger].map(
+                index => keypoints[index]
+            )
+            const region = new Path2D()
+            const [{ x, y }] = points
+
+            region.moveTo(x, y)
+
+            for(const point of points) {
+                region.lineTo(point.x, point.y)
+            }
+
+            this.#canvasContext.strokeStyle = "blue"
+            this.#canvasContext.stroke(region)
         }
     }
 
